a578b594f8324a372a93bc8a69d43738fbe21249
menghorHou <mhour3055@gmail.com>
2025-07-19 16:38:29 +0700

    role api

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:50,unique:roles,name',
+            'status' => 'required|boolean|in:1,0',
+            'description' => 'nullable|string'
+        ]);
+        $role = new Role();
+        $role->name = $request->input('name');
+        $role->status = $request->input('status');
+        $role->description = $request->input('description');
+        $role->save();
+        return response()->json([
+            "result" => true,
+            "message" => "Role created successfully!",
+            "data" => $role
+        ]);
+    }
+
+    public function index(Request $request)
+    {
+        $request->validate([
+            "column" => "nullable|exists:roles,id,name",
+            "sort" => "nullable|in:desc,asc",
+        ]);
+        $column = $request->input('column') ?? 'id';
+        $sort = $request->input('sort') ?? 'asc';
+        $role = new Role();
+        if ($request->filled('search'))
+        {
+            $role = $role->where('id','like','%'.$request->input('search').'%');
+            // $role = $role->orWhere('name','like','%'.$request->input('search').'%');
+        }
+        $role = $role->orderBy($column,$sort)->get();
+
+        return response()->json([
+            "result" => true,
+            "message" => "Successfully!",
+            "data" => $role
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        
+    }
+}
