cc1720a03387fcfbbe0ad61da7bac532fb3a7c43
menghorHou <mhour3055@gmail.com>
2025-07-31 00:38:15 +0700

    remove /vendor

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
@@ -16,5 +16,5 @@ Route::get('/user',[UserController::class,'index']);
 Route::prefix('/auth')->group(function () {
     Route::post('/register',[AuthController::class,'register']);
     Route::post('/login',[AuthController::class,'login']);
-    Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:api'); // Change middleware to 'auth:api'
+    Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:api');
 });
