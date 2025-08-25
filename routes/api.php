d460bd8522e065a82fe97041798037ed5fbd3928
menghorHou <mhour3055@gmail.com>
2025-08-25 13:31:47 +0700

    route api

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,22 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 
-// This group handles all your API routes without adding an extra /api prefix.
-Route::group(['middleware' => ['api']], function () {
-    Route::post('/role', [RoleController::class, 'store']);
-    Route::get('/role', [RoleController::class, 'index']);
-    Route::delete('/role/{id}', [RoleController::class, 'destroy']);
-    Route::put('/role/{id}', [RoleController::class, 'update']);
+Route::group(['prefix' => ''], function () {
+  Route::post('/role',[RoleController::class,'store']);
+  Route::get('/role',[RoleController::class,'index']);
+  Route::delete('/role/{id}',[RoleController::class,'destroy']);
+  Route::put('/role/{id}',[RoleController::class,'update']);
+});
+Route::get('/user',[UserController::class,'index']);
 
-    Route::get('/user', [UserController::class, 'index']);
-
-    // Your auth routes will be handled directly here.
-    Route::post('/auth/register', [AuthController::class, 'register']);
-    Route::post('/auth/login', [AuthController::class, 'login']);
-    Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:api');
-    Route::post('/auth/forget_password', [AuthController::class, 'forgetPassword']);
+Route::prefix('/auth')->group(function () {
+    Route::post('/register',[AuthController::class,'register']);
+    Route::post('/login',[AuthController::class,'login']);
+    Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:api');
+    Route::post('/forget_password', [AuthController::class, 'forgetPassword']);
 });
